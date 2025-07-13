ceec27aafa6b34113da785fafa9d4c2a3c27ba70
Sourav <souravbhardwaj997@gmail.com>
2025-07-13 10:53:03 +0530

    Set up some middlewares

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,19 @@ import dotenv from "dotenv";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+
 dotenv.config();
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 //middlewares
 app.use(express.json());
 app.use(cors());
-app.use(helmet());
+app.use(helmet()); // secure our app by providing various headers
+app.use(morgan("dev")); // log the request to us
+
+//running the server
+
 app.listen(PORT, () => {
   console.log("Server is running at PORT ", PORT);
 });
